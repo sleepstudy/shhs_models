f31b428239da012118cbcf69e5b72b64bb63b67b
Alastair Paragas <alastairparagas@gmail.com>
2019-04-28 15:37:56 -0700

    Bugfixes, Random Forest PCA

--- random_forest/main.py
+++ random_forest/main.py
@@ -5,6 +5,5 @@ from common.utils import pipeline
 if __name__ == '__main__':
   pipeline(
     RandomForestClassifier(64), 
-    csv_path="./5patients.csv", 
-    scoring_metric_class=AllScoringMetric
+    csv_path="./5patients.csv"
   )
---
+++ random_forest_pca/main.py
@@ -0,0 +1,27 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
+from common.utils import pipeline
+
+
+class RandomForestPCA:
+  
+  model = RandomForestClassifier(64)
+  dim_reducer = None
+  
+  def fit(self, x_train, y_train):
+    self.dim_reducer = PCA(n_components=10)
+    transformed = self.dim_reducer.fit_transform(x_train)
+    self.model.fit(transformed, y_train)
+    
+  def predict(self, x_test):
+    if self.dim_reducer is None:
+      raise ValueError("Not trained!")
+    transformed = self.dim_reducer.transform(x_test)
+    return self.model.predict(transformed)
+    
+    
+if __name__ == '__main__':
+  pipeline(
+    RandomForestPCA(), 
+    csv_path="./5patients.csv"
+  )
